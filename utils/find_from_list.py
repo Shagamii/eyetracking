ab1a966ed925dac11322bbe36d931501ad9f160e
Shagamii <shagamiiiii@gmail.com>
2019-10-30 18:52:38 +0900

    utils/find_from_list

---
+++ utils/find_from_list.py
@@ -0,0 +1,13 @@
+# https://docs.python.org/ja/3/library/itertools.html 
+def find_from_list(iterable, callback, default=None):
+    """Returns the first true value in the iterable.
+
+    If no true value is found, returns *default*
+
+    If *pred* is not None, returns the first item
+    for which pred(item) is true.
+
+    """
+    # first_true([a,b,c], x) --> a or b or c or x
+    # first_true([a,b], x, f) --> a if f(a) else b if f(b) else x
+    return next(filter(callback, iterable), default)
