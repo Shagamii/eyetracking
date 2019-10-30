dc2181af279161a0ba6dc050fd4a4404a5caa4bb
Shagamii <shagamiiiii@gmail.com>
2019-10-30 18:52:02 +0900

    CSV_HEADERを変数化

--- output_to_csv.py
+++ output_to_csv.py
@@ -1,16 +1,16 @@
-from os.path import join, dirname
 import csv
-import asyncio
+from os.path import join, dirname
 
+CSV_HEADER = [
+    'time_stamp',
+    'left_gaze_point_x',
+    'left_gaze_point_y',
+    'right_gaze_point_x',
+    'right_gaze_point_y'
+]
 
 def output_to_csv(gaze_data_list, _path):
     csv_path = join(dirname(__file__), "..", "data" , _path + ".csv")
     with open(csv_path, 'w', newline="") as f:
-        writer = csv.DictWriter(f, [
-            'time_stamp',
-            'left_gaze_point_x',
-            'left_gaze_point_y',
-            'right_gaze_point_x',
-            'right_gaze_point_y'
-            ])
+        writer = csv.DictWriter(f, CSV_HEADER)
         writer.writerows(gaze_data_list)
